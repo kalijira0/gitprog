7288ff8da1f327327f81b3af6b140b8fb5f26926
kalijira0 <kalijira0@gmail.com>
2022-08-22 19:58:39 +0530

    branch3 added code

--- src/org/git/Git.java
+++ src/org/git/Git.java
@@ -2,6 +2,11 @@ package org.git;
 
 public class Git {
 
+	public void m3()
+	{
+		System.out.println("branch3 code");
+	}
+	
 	public static void main(String args[])
 	{
 		System.out.println("Hello world");
