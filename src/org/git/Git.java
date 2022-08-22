31517fce5c03ef972f7d15ba930583275b365659
kalijira0 <kalijira0@gmail.com>
2022-08-22 12:34:46 +0530

    branch 2 added code

--- src/org/git/Git.java
+++ src/org/git/Git.java
@@ -2,6 +2,10 @@ package org.git;
 
 public class Git {
 
+	public void m2()
+	{
+		System.out.println("branch2 code");
+	}
 	public static void main(String args[])
 	{
 		System.out.println("Hello world");
